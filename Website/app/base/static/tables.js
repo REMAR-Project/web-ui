67cce36b1e43d0e1344a5d05b27328b4c523ebdd
Zoe <zoe@zwall.co.uk>
2018-07-04 00:13:24 +0100

    started on front page

--- Website/app/base/static/tables.js
+++ Website/app/base/static/tables.js
@@ -2,10 +2,39 @@ $(document).ready(function() {
 
     console.log("tables")
 
-    $('#settingsBtn').click(buttonDude);
+    $('#allVersBtn').click(showAll);
+
+    $("#shortVBtn").click(function() {
+        $.fn.dataTable.ext.search.pop(); // pop first to get rid of any filters
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                return (data[1] == '0');
+            });
+        coolTable.draw();
+        var toHide = [ 1, 9, 10, 11, 12, 13, 14, 15, 16]
+        coolTable.columns().visible(true);
+        for(ind of toHide)
+        {
+            coolTable.column(ind).visible(false);
+        }
+    });
+
+    $("#longVBtn").click(function() {
+        $.fn.dataTable.ext.search.pop(); // pop first to get rid of filters
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                return (data[1] == '1');
+            });
+        coolTable.draw();
+        coolTable.columns().visible(true);
+        coolTable.column(1).visible(false);
+    });
+      
 })
 
-function buttonDude(){
-	coolTable.column(1).visible(false);
+function showAll(){
+    coolTable.columns().visible(true); // show all columns
+    $.fn.dataTable.ext.search.pop();
+    coolTable.draw();
 }
 
--- Website/app/home/routes.py
+++ Website/app/home/routes.py
@@ -2,11 +2,20 @@ from app.home import blueprint
 from flask import render_template
 from flask_login import login_required
 
+import requests
+import sys
 
 @blueprint.route('/index')
 @login_required
 def index():
-    return render_template('index.html')
+    
+    print("hello world", file=sys.stderr)
+    # total number of submissions
+    dbOverview = requests.get('http://localhost:5984/cleancrab/')
+    
+    print (dbOverview.json()['doc_count'], file=sys.stderr)
+
+    return render_template('index.html', docCount=dbOverview.json()['doc_count'])
 
 
 @blueprint.route('/<template>')
