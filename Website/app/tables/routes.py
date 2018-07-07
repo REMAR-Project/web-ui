f52fea3c79711ad5d4a967ffdb1f8778e9c3db8f
Zoe <zoe@zwall.co.uk>
2018-07-07 21:50:03 +0100

    fixed protected areas

--- Website/app/tables/routes.py
+++ Website/app/tables/routes.py
@@ -6,7 +6,7 @@ from flask_login import login_required
 import json
 import sys #for print
 import requests
-
+from collections import OrderedDict
 
 @blueprint.route('/<template>')
 @login_required
@@ -15,22 +15,18 @@ def route_template(template):
     # get all docs
     myjson = requests.get('http://localhost:5984/cleancrab/_all_docs?include_docs=true')
 
+    # get regions from db
+    regionjson = requests.get('http://localhost:5984/regions/_all_docs?include_docs=true')
+
     # for each id, save entry
     #print(myjson.json(), file=sys.stderr)
     entries = []
     for element in myjson.json()['rows']:
         entries.append(element['doc'])
     
-    return render_template(template + '.html', test=entries)
+    regions = json.loads(regionjson.text, object_pairs_hook=OrderedDict)
+    regDoc = regions['rows'][0]['doc']
 
+    return render_template(template + '.html', test=entries, regions=regDoc)
 
-
-
-def testFun():
-    #my_dict["hello"] = "hello"
-    myjson = requests.get('http://localhost:5984/cleancrab/2018-01-19T16-20-22_2cd088ea-577d-4729-9870-ef68d93c8bfc.json')
-   # my_dict["me"] = response
-   # my_dict["gb"] = "Goodbye"
-    print("lol", file=sys.stderr)
-    print(myjson.content, file=sys.stderr)
-    print("h", file=sys.stderr)
+    
